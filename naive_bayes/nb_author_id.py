fef5915e05ddd03a8ba3df1b146bfe19c89f084c
Nguyen Luong An Phu <anphunl@gmail.com>
2016-08-29 00:39:02 +0700

    Finish naive bayes

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -26,6 +26,14 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+classifier = GaussianNB()
+classifier.fit(features_train, labels_train)
+
+pred = classifier.predict(features_test)
+print ("Accuracy:", accuracy_score(labels_test, pred))
+
 
 #########################################################
 
