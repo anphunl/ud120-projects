24b50840f57669da16161d83b584e323bf7d79b4
Nguyen Luong An Phu <anphunl@gmail.com>
2016-09-04 21:03:27 +0700

    Decision tree

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -25,6 +25,16 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+
+acc_min_samples_split_40 = accuracy_score(labels_test, pred)
+print ('Accuracy when min split = 40:', acc_min_samples_split_40)
+print ('Number of features:', len(features_train[0]))
 
 #########################################################
 
