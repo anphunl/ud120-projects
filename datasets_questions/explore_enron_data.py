ff0455acffd5cea40a083de3f79939d8b100cbce
Nguyen Luong An Phu <anphunl@gmail.com>
2016-09-07 23:10:11 +0700

    Count number of POI and features

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -17,6 +17,9 @@
 
 import pickle
 
-enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
+enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "rb"))
 
 
+print ("Number of people:", len(enron_data))
+print ("Number of features:", len(enron_data['GARLAND C KEVIN']))
+print ("Number of POIs:", sum(1 for x in enron_data if enron_data[x]['poi'] == 1))
